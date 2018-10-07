628669149854017e527e6c3ece49c952052a72c8
Alejandro Napoles <alejandro@alejandronapoles.com>
2018-10-07 12:59:43 +0100

    Added babel-plugin-styled-components and remark-images-contentful plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,10 +33,13 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          'gatsby-remark-prismjs',
           {
-            resolve: `gatsby-remark-images`,
+            resolve: `gatsby-remark-images-contentful`,
             options: {
-              maxWidth: 590,
+              maxWidth: 650,
+              backgroundColor: 'white',
+              linkImagesToOriginal: false,
             },
           },
           {
@@ -45,8 +48,6 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
       },
