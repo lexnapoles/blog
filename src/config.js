83a13c58dc88caea3394b098c386084791eaae6f
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2025-09-20 11:54:30 +0100

    Update site description

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "Alejandro Napoles";
-export const SITE_DESCRIPTION = "My personal site about software development";
+export const SITE_DESCRIPTION = "My personal site about software development and other random things";
